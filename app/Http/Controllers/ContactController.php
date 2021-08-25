22d27a3b743f195b23aabdb052ec1b38738d85b2
DavutUdemy <61990003+DavutUdemy@users.noreply.github.com>
2021-08-25 19:05:03 +0400

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -38,8 +38,13 @@ class ContactController extends Controller
         if ($checkIfContactExists === null) {
             return response("There is not a contact with this id", 500);
         }
-        $this->deleteById($request->id);
-        $this->updateMethod($request, $request->id);
+          $update =  Appointments::find($request->id);
+        $update->user_id = $request->get('user_id');
+        $update->firstName = $request->get('firstName');
+        $update->lastName = $request->get('lastName');
+        $update->email_Address = $request->get('email_Address');
+        $update->return_time = $request->get('phone_Number');
+
         return response("Successfully,Your appointment updated");
 
 
@@ -96,34 +101,5 @@ class ContactController extends Controller
 
     }
 
-    public function updateMethod(Request $request, $id)
-    {
-        $validator = Validator::make($request->all(), [
-            //Burda Validator ile Validation kuralarimizi yazdik
-            'firstName' => 'required|string|max:255',
-            'lastName' => 'required|string|max:255',
-            'email_Address' => 'required|string|max:255'
-        ]);
-
-
-        //Eger validasyon kuralarina uymuyorsak hata yiyoruz
-        if ($validator->fails()) {
-            //Hatamizin icerigi ve statusunu belirliyoruz
-            return response()->json($validator->errors()->toJson(), 400);
-        }
-        //Burda da artik validasyondan gecmis datalarimizi veirtabanina kaydediyoruz
-        $contact = Contacts::create([
-            'id' => $id,
-            'user_id' => $request->get('user_id'),
-            'firstName' => $request->get('firstName'),
-            'lastName' => $request->get('lastName'),
-            'email_Address' => $request->get('email_Address'),
-            'phone_Number' => $request->get('phone_Number'),
-        ]);
-
-        //Son olarak basarili mesajini veriyoruz
-        return response("Succesufully,Your contact information saved");
-
-
-    }
+     
 }
